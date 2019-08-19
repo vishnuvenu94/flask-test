b3854a157470842761569a76e17a145996b55d3c
vishnu venu <vishnuvenuvenk@gmail.com>
2019-08-19 13:34:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+# from flask_marshmallow import marshmalllow
+import os
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+db = SQLAlchemy(app)
+
+@app.route("/")
+def entry():
+    return "working"
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
