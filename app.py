e60f8b0e47441827ae11627d1771f910abd200d3
vishnu venu <vishnuvenuvenk@gmail.com>
2019-08-19 13:50:26 +0530

    removed sqlalchemy

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+
 # from flask_marshmallow import marshmalllow
 import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-db = SQLAlchemy(app)
 
 @app.route("/")
 def entry():
     return "working"
-
-
-
-
-
 
 
 if __name__ == "__main__":
