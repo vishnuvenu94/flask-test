4d7ce4a6d655803cb66b2a152a82dc261c5d54e5
vishnu venu <vishnuvenuvenk@gmail.com>
2019-08-21 16:39:19 +0530

    clean up

--- app.py
+++ app.py
@@ -3,78 +3,14 @@ import json
 from waitress import serve
 from graphqlclient import GraphQLClient
 
-# from flask_marshmallow import marshmalllow
-import os
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
+
 PORT = 8080
 
 graphqlClient = GraphQLClient("https://hasura-sa.cap.jaagalabs.com/v1/graphql")
 graphqlClient.inject_token(
     "1SocialAlpha", 'x-hasura-admin-secret')
-
-user_id = 12
-
-problems = [
-    {
-        "title": "flask test 10",
-        "description": "Sample article content",
-        "user_id": 5,
-        "max_population": 2323,
-        "organization": "wewe"
-    },
-    {
-        "title": "flask test 12",
-        "description": "Sample article content",
-        "user_id": 5,
-        "max_population": 2323,
-        "organization": "wewe"
-    }
-]
-
-problems_update_query = '''
-    {
-    problems(where:{id:{_eq:%s}}){
-
-     problem_owners{
-      user_id
-    }
-    problem_watchers{
-      user_id
-    }
-    problem_validations{
-      user_id
-    }
-    problem_collaborators{
-      user_id
-    }
-
-
-    }
-    }
-    ''' % user_id
-
-problems_insert_query = '''
- {
-    problems(where:{id:{_eq:%s}}){
-         problems_tags{
-        tag{
-          users_tags{
-            user_id
-            tag_id
-          }
-        }
-      }
-
-
-
-
-    }
-    }'''
-
-# test = json.dumps(problems)
-# print(test)
 
 
 notifications_insert_mutation = '''
@@ -91,52 +27,27 @@ mutation insert_notifications($objects: [notifications_insert_input!]! ) {
 '''
 
 
-test = [{"problem_id": 69, "user_id": 5, "tag_id": 4},
-        {"problem_id": 69, "user_id": 5, "tag_id": 3}]
-# .replace('$problems', json.dumps([{"title": "from flask api", "description": "ddasdasd", "max_population": 21, "user_id": 5}]))
-
-
 @app.route("/")
 def entry():
-    # file = open("myfile.txt", "w+")
-    # print(json.loads(graphqlClient.execute(query)))
-    # print(mutation)
-    # graphqlClient.execute(problems_insert_mutation)
-    # graphqlClient.execute(problems_insert_mutation, {
-    #                       'objects': list(problems)})
-
-    # json_acceptable_string = payload.replace("'", "\"")
-
-    # d = json.loads(json_acceptable_string)
-    # print(d["data"]["problems"])
-    # graphqlClient.execute(notifications_insert_mutation, {
-    #     'objects': list(test)})
-
-    # file.close()
 
     print("testing")
     return "working"
 
 
 @app.route("/problems", methods=['POST'])
-def handle_problem_event_trigger():
-    # users_to_notify_on_insert = []
+def handle_problem_events():
+
     users_to_notify_on_update = []
     problem_insert_notifications = []
     problem_update_notifications = []
 
-    # print(request)
-    # file = open("myfile.txt", "w+")
-    # file.write(json.dumps(request.json))
-    # file.close()
-    # return json.dumps(request.json)
-    print(request.json, "====request", type(request.json))
+    # print(request.json, "====request", type(request.json))
 
     trigger_payload = request.json
-    print(trigger_payload, "notification payload",
-          type(trigger_payload))
+    # print(trigger_payload, "notification payload",
+    #       type(trigger_payload))
     problem_id = trigger_payload["event"]["data"]["new"]["id"]
-    print("problem id==========", problem_id)
+    # print("problem id==========", problem_id)
     user_id = trigger_payload["event"]["data"]["new"]["user_id"]
 
     if trigger_payload["event"]["op"] == "INSERT":
@@ -161,8 +72,10 @@ def handle_problem_event_trigger():
             for tags in values:
                 for tag in tags["tag"]["users_tags"]:
                     # print(tag, "tag")
-                    problem_insert_notifications.append(
-                        {"user_id": tag["user_id"], "tag_id": tag["tag_id"], "problem_id": problem_id})
+                    if(tag["user_id"] != user_id):
+
+                        problem_insert_notifications.append(
+                            {"user_id": tag["user_id"], "tag_id": tag["tag_id"], "problem_id": problem_id})
         print("user to notify ========", problem_insert_notifications)
         try:
             graphqlClient.execute(notifications_insert_mutation, {
