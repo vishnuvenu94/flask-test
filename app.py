2e21cbb4aee892e95c1f0081f4f754d2939ab4cb
vishnu venu <vishnuvenuvenk@gmail.com>
2019-08-21 15:17:57 +0530

    problem update trigger handled

--- app.py
+++ app.py
@@ -91,19 +91,6 @@ mutation insert_notifications($objects: [notifications_insert_input!]! ) {
 '''
 
 
-problems_insert_mutation = '''
-mutation insert_problems($objects: [problems_insert_input!]! ) {
-    insert_problems(
-        objects:$objects
-    ) {
-        returning {
-            id
-
-        }
-    }
-}
-'''
-
 test = [{"problem_id": 69, "user_id": 5, "tag_id": 4},
         {"problem_id": 69, "user_id": 5, "tag_id": 3}]
 # .replace('$problems', json.dumps([{"title": "from flask api", "description": "ddasdasd", "max_population": 21, "user_id": 5}]))
@@ -132,8 +119,12 @@ def entry():
 
 
 @app.route("/problems", methods=['POST'])
-def handleEvents():
-    users_to_notify = []
+def handle_problem_event_trigger():
+    # users_to_notify_on_insert = []
+    users_to_notify_on_update = []
+    problem_insert_notifications = []
+    problem_update_notifications = []
+
     # print(request)
     # file = open("myfile.txt", "w+")
     # file.write(json.dumps(request.json))
@@ -141,14 +132,14 @@ def handleEvents():
     # return json.dumps(request.json)
     print(request.json, "====request", type(request.json))
 
-    notification_payload = request.json
-    print(notification_payload, "notification payload",
-          type(notification_payload))
-    problem_id = notification_payload["event"]["data"]["new"]["id"]
+    trigger_payload = request.json
+    print(trigger_payload, "notification payload",
+          type(trigger_payload))
+    problem_id = trigger_payload["event"]["data"]["new"]["id"]
     print("problem id==========", problem_id)
-    user_id = notification_payload["event"]["data"]["new"]["user_id"]
+    user_id = trigger_payload["event"]["data"]["new"]["user_id"]
 
-    if notification_payload["event"]["op"] == "INSERT" or "UPDATE":
+    if trigger_payload["event"]["op"] == "INSERT":
         problems_insert_query = '''
             {
               problems(where:{id:{_eq:%s}}){
@@ -170,13 +161,51 @@ def handleEvents():
             for tags in values:
                 for tag in tags["tag"]["users_tags"]:
                     # print(tag, "tag")
-                    users_to_notify.append(
+                    problem_insert_notifications.append(
                         {"user_id": tag["user_id"], "tag_id": tag["tag_id"], "problem_id": problem_id})
-        print("user to notify ========", users_to_notify)
+        print("user to notify ========", problem_insert_notifications)
         try:
-
             graphqlClient.execute(notifications_insert_mutation, {
-                'objects': list(users_to_notify)})
+                'objects': list(problem_insert_notifications)})
+        except:
+            pass
+    elif (trigger_payload["event"]["op"] == "UPDATE"):
+
+        problems_update_query = '''
+                        {
+             problems(where:{id:{_eq:%s}}){
+
+            problem_owners{
+            user_id
+            }
+            problem_watchers{
+            user_id
+            }
+            problem_validations{
+            user_id
+            }
+            problem_collaborators{
+            user_id
+            }
+
+
+            }
+            }
+        ''' % (problem_id)
+        problem_update_query_data = json.loads(graphqlClient.execute(problems_update_query))[
+            "data"]["problems"][0]
+        for item, values in problem_update_query_data .items():
+            for value in values:
+
+                users_to_notify_on_update.append(value["user_id"])
+
+        users_to_notify_on_update = list(set(users_to_notify_on_update))
+        for user in users_to_notify_on_update:
+            notifification_entry = {"user_id": user, "problem_id": problem_id}
+            problem_update_notifications.append(notifification_entry)
+        try:
+            graphqlClient.execute(notifications_insert_mutation, {
+                'objects': list(problem_update_notifications)})
         except:
             pass
 
